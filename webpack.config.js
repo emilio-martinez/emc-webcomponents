5423d02eb612f5b5385269741c7b4684abfaa3cf
Emilio Martinez <e.marticor@gmail.com>
2018-04-15 03:44:48 -0700

    build(webpack): watch element template files

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
-const webpack = require('webpack');
 const path = require('path');
 const util = require('util');
 const fs = require('mz/fs');
+const globby = require('globby');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 const LicenseWebpackPlugin = require('license-webpack-plugin').LicenseWebpackPlugin;
+
+const elementsPath = 'src/elements';
+
+function nicerLog(title, ...logItems) {
+  const inspectOpts = { colors: true, compact: true };
+  console.log(logItems);
+  const toPrint = logItems.reduce((acc, arg) => acc.concat(util.inspect(arg, inspectOpts), '\r\n'), []);
+  console.log(toPrint);
+  console.log(`${title}:\r\n`, ...toPrint);
+}
 
 function htmlWebpackPluginChunksSortMode(a, b) {
   return a.names[0] === 'vendor' ? -1 : b.names[0] === 'vendor' ? 1 : 0;
@@ -27,7 +37,7 @@ function deleteDirRecursively(path) {
 }
 
 async function getElements() {
-  const elementsDir = path.resolve(__dirname, 'src/elements');
+  const elementsDir = path.resolve(__dirname, elementsPath);
   const elementPathNames = await fs.readdir(elementsDir).catch(console.error);
 
   if (!elementPathNames) return { elementData: [], elementEntries: [], elementHTMLPlugins: [] };
@@ -64,9 +74,36 @@ async function getElements() {
       })
   );
 
-  console.info('Following elements found:\r\n', elementData, '\r\n');
+  nicerLog('Following elements found', elementData);
 
   return { elementData, elementEntries, elementHTMLPlugins };
+}
+
+class WatchExternalFilesPlugin {
+  constructor({ files = [], verbose = false } = {}) {
+    this.files = files;
+    this.verbose = !!verbose;
+    this._firstRun = false;
+  }
+
+  apply(compiler) {
+    compiler.hooks.afterCompile.tapAsync('watch-external-files-plugin', async (compilation, callback) => {
+      const filePaths = await globby(this.files, { absolute: true });
+
+      if (this.verbose && !this._firstRun) {
+        nicerLog('Watching external files', filePaths);
+      }
+
+      filePaths.forEach(file => {
+        Array.isArray(compilation.fileDependencies)
+          ? compilation.fileDependencies.push(file)
+          : compilation.fileDependencies.add(file);
+      });
+
+      this._firstRun = true;
+      callback();
+    });
+  }
 }
 
 module.exports = async (_, argv) => {
@@ -146,6 +183,10 @@ module.exports = async (_, argv) => {
       ...elementHTMLPlugins,
       new CompressionPlugin({
         include: /\.js$/
+      }),
+      new WatchExternalFilesPlugin({
+        files: [`./${elementsPath}/**/*.html`],
+        verbose: true
       })
     ],
 
